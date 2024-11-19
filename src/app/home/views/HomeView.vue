1421eb6407663710c98534082dfda4598c315237
Joel Ccasane <joelccasane@gmail.com>
2024-11-19 15:51:08 -0500

    update header

--- src/app/home/views/HomeView.vue
+++ src/app/home/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
+  <AppHeader></AppHeader>
   <main></main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import '@fontsource-variable/lora';
+import '@fontsource/anton';
+import AppHeader from '@/app/shared/AppHeader.vue';
+</script>
--- src/app/shared/AppHeader.vue
+++ src/app/shared/AppHeader.vue
@@ -1,7 +1,62 @@
 <template>
-  <header></header>
+  <header class="header">
+    <div class="header__container">
+      <div class="header__logo">
+        <a href="/">MEF.</a>
+      </div>
+      <nav class="header__nav">
+        <a href="/">Inicio</a>
+        <a href="/">Película</a>
+        <a href="/">Acerca de</a>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.header {
+  padding: 1.2rem 1rem;
+  background-color: $primary-color;
+
+  &__container {
+    max-width: 990px;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__logo {
+    background-color: $neutral-color;
+    padding: 0.2rem 0.8rem;
+
+    a {
+      font-size: 1.6rem;
+      font-family: $font-title;
+    }
+
+    &:hover {
+      background-color: $accent-color;
+    }
+  }
+
+  &__nav {
+    display: inline-flex;
+    align-items: center;
+    gap: 2rem;
+
+    a {
+      color: $neutral-color;
+      font-weight: 600;
+
+      &:hover {
+        text-decoration: underline;
+        text-underline-offset: 2.5px;
+        text-decoration-thickness: 1.5px;
+      }
+    }
+  }
+}
+</style>
