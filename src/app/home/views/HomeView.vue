14ff3030b57348d0ba7a10586c04e86160e65e8f
Joel Ccasane <joelccasane@gmail.com>
2024-11-23 16:58:09 -0500

    update footer

--- src/app/home/views/HomeView.vue
+++ src/app/home/views/HomeView.vue
@@ -4,6 +4,7 @@
     <HomeIntro></HomeIntro>
     <HomeFilmRolls></HomeFilmRolls>
   </main>
+  <AppFooter></AppFooter>
 </template>
 
 <script setup lang="ts">
@@ -12,4 +13,5 @@ import '@fontsource-variable/lora';
 import AppHeader from '@/app/shared/AppHeader.vue';
 import HomeIntro from '../sections/HomeIntro.vue';
 import HomeFilmRolls from '../sections/HomeFilmRolls.vue';
+import AppFooter from '@/app/shared/AppFooter.vue';
 </script>
--- src/app/shared/AppFooter.vue
+++ src/app/shared/AppFooter.vue
@@ -1,7 +1,54 @@
 <template>
-  <footer></footer>
+  <footer class="footer">
+    <div class="footer__container">
+      <p>
+        © {{ dateFooter }} Memorias en Film. Realizado por
+        <a href="/">Joel Ccasane</a>
+      </p>
+      <div class="footer__links">
+        <a href="">Inicio</a>
+        <a href="">Carretes</a>
+        <a href="">Acerca de</a>
+      </div>
+    </div>
+  </footer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+const dateFooter = ref(new Date().getFullYear());
+</script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.footer {
+  padding: 1.8rem 1rem;
+  background-color: $accent-color;
+
+  &__container {
+    max-width: 990px;
+    width: 100%;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    p {
+      font-size: 0.875rem;
+    }
+
+    a:hover {
+      text-decoration: underline;
+      text-underline-offset: 2.5px;
+      text-decoration-thickness: 1.5px;
+    }
+  }
+
+  &__links {
+    display: inline-flex;
+    align-items: center;
+    gap: 1.5rem;
+    font-weight: 600;
+    font-size: 0.875rem;
+  }
+}
+</style>
