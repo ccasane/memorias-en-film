010b958b5159d85e1d00b1cdb0f4c5040522f9d1
Joel Ccasane <joelccasane@gmail.com>
2025-02-25 20:40:20 -0500

    update header

--- src/app/shared/AppHeader.vue
+++ src/app/shared/AppHeader.vue
@@ -5,51 +5,31 @@
         <RouterLink to="/">MEF.</RouterLink>
       </div>
 
-      <div class="header__hamburger" @click="toggleMenu">
-        <span class="header__hamburger-bar"></span>
-        <span class="header__hamburger-bar"></span>
-        <span class="header__hamburger-bar"></span>
+      <div class="header__hamburger">
+        <button id="menu-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <div class="header__menu">
+          <div class="menu">
+            <RouterLink to="/">Incio</RouterLink>
+            <RouterLink to="film-rolls">Carretes</RouterLink>
+            <RouterLink to="about">Acerca de</RouterLink>
+          </div>
+        </div>
       </div>
 
-      <nav ref="menu" :class="['header__nav', { 'header__nav--active': menuOpen }]">
-        <RouterLink to="/" @click="closeMenu">Inicio</RouterLink>
-        <RouterLink to="film-rolls" @click="closeMenu">Carretes</RouterLink>
-        <RouterLink to="about" @click="closeMenu">Acerca de</RouterLink>
+      <nav class="header__nav">
+        <RouterLink to="/">Inicio</RouterLink>
+        <RouterLink to="film-rolls">Carretes</RouterLink>
+        <RouterLink to="about">Acerca de</RouterLink>
       </nav>
     </div>
   </header>
 </template>
 
-<script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount } from 'vue';
-
-const menuOpen = ref(false);
-
-const toggleMenu = () => {
-  menuOpen.value = !menuOpen.value;
-};
-
-const closeMenu = () => {
-  menuOpen.value = false;
-};
-
-const handleClickOutside = (event: MouseEvent) => {
-  const menu = document.querySelector('.header__nav');
-  const hamburger = document.querySelector('.header__hamburger');
-
-  if (!menu?.contains(event.target as Node) && !hamburger?.contains(event.target as Node)) {
-    menuOpen.value = false;
-  }
-};
-
-onMounted(() => {
-  document.addEventListener('click', handleClickOutside);
-});
-
-onBeforeUnmount(() => {
-  document.removeEventListener('click', handleClickOutside);
-});
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss" scoped>
 .header {
@@ -86,22 +66,76 @@ onBeforeUnmount(() => {
   }
 
   &__hamburger {
-    width: 30px;
-    height: 22px;
+    position: relative;
     display: none;
-    flex-direction: column;
-    gap: 4px;
-    cursor: pointer;
-    justify-content: space-between;
 
-    .header__hamburger-bar {
-      width: 100%;
-      height: 4px;
-      background-color: $primary-color;
+    #menu-toggle {
+      display: flex;
+      justify-content: space-between;
+      flex-direction: column;
+      width: 40px;
+      height: 30px;
+      cursor: pointer;
+      border: none;
+      background-color: transparent;
+
+      span {
+        display: block;
+        width: 100%;
+        height: 6px;
+        background-color: $primary-color;
+      }
+
+      @media (max-width: 768px) {
+        &:focus ~ .header__menu {
+          width: 250px;
+          z-index: 99;
+        }
+      }
     }
 
     @media (max-width: 768px) {
+      display: initial;
+    }
+  }
+
+  &__menu {
+    position: fixed;
+    max-width: 300px;
+    top: 0;
+    right: -2px;
+    bottom: 0;
+    height: 100%;
+    width: 0;
+    overflow: hidden;
+    z-index: 1;
+    border-left: 4px solid;
+    background: $accent-color;
+    transition: all 0.29s ease-in-out;
+
+    .menu {
       display: flex;
+      flex-direction: column;
+      gap: 1.5rem;
+      padding: 1.8rem;
+
+      a {
+        font-size: 1.3rem;
+        font-weight: 600;
+
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+
+    @media (max-width: 767px) {
+      width: 0;
+
+      &:target {
+        width: 250px;
+        z-index: 99;
+      }
     }
   }
 
@@ -122,29 +156,7 @@ onBeforeUnmount(() => {
     }
 
     @media (max-width: 768px) {
-      position: fixed;
-      top: 0;
-      width: 300px;
-      height: 100%;
-      right: -2px;
-      background-color: $accent-color;
-      display: flex;
-      flex-direction: column;
-      align-items: start;
-      border-left: 3px solid $primary-color;
-      gap: 1.5rem;
-      padding: 1.5rem;
-      transform: translateX(100%);
-      transition: transform 0.3s ease-in-out;
-      z-index: 999;
-
-      &--active {
-        transform: translateX(0);
-      }
-
-      a {
-        font-size: 1.3rem;
-      }
+      display: none;
     }
   }
 }
